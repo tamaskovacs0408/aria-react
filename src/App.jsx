e054047f6ca7d01276c6446b260ffda233eb3579
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-07 20:31:27 +0200

    Add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
-import Accordion from "./components/accordion/Accordion";
+import Accordion from "./components/Accordion/Accordion";
+import AlertDialog from "./components/AlertDialog/AlertDialog";
 
 const routerSettings = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Accordion /> }],
+    children: [
+      { index: true, element: <Accordion /> },
+      { path: "alertdialog", element: <AlertDialog /> },
+    ],
   },
 ]);
 
