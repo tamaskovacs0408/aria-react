b36be2943062e420e31d60aae40bf31d35cd1bd9
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-07 19:09:37 +0200

    Add routes via React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Accordion from './components/accordion/Accordion'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Accordion from "./components/accordion/Accordion";
+
+const routerSettings = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ index: true, element: <Accordion /> }],
+  },
+]);
 
 function App() {
-
-  return (
-    <main>
-      <Accordion />
-    </main>
-  )
+  return <RouterProvider router={routerSettings} />;
 }
 
-export default App
+export default App;
