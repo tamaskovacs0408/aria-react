5edf728a0a2b8ebc687810a61af546d537227140
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-07 20:31:12 +0200

    Change Link to NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.scss"
 
 export default function Navbar() {
   return (
         <nav className={classes.navbar}>
             <ul>
-                <li><Link to="/">Accordion</Link></li>
+                <li><NavLink to="/" end>Accordion</NavLink></li>
+                <li><NavLink to="/alertdialog">Alertdialog</NavLink></li>
             </ul>
         </nav>
   )
