9846a341518d5acdd4d23a5e2e370c95db058c5f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-09 19:56:43 +0200

    Pass props and add onclick function

--- src/components/AlertDialog/AlertDialogModal.jsx
+++ src/components/AlertDialog/AlertDialogModal.jsx
@@ -1,20 +1,26 @@
 import classes from "./AlertDialogModal.module.scss";
 
-export default function AlertDialogModal ({ isModalOpen, modalHandler, textAreaValue }) {
+export default function AlertDialogModal({
+  isModalOpen,
+  modalHandler,
+  textAreaValue,
+  deleteNotes
+}) {
+  let notes = textAreaValue.value;
 
-    let notes = textAreaValue.value;
-
-
-    return (
-      isModalOpen && (
-        <div className={classes["modal-wrapper"]}>
-          <div className={classes["modal-container"]}>
-            <h2>Confirmation</h2>
-            <p>Are you sure want to discard all of your notes?</p>
-            <p>{notes.split(" ").length} words will be deleted.</p>
-            <button onClick={modalHandler}>Close</button>
+  return (
+    isModalOpen && (
+      <div className={classes["modal-wrapper"]}>
+        <div className={classes["modal-container"]}>
+          <h2>Confirmation</h2>
+          <p>Are you sure want to discard all of your notes?</p>
+          <p>{notes.split(" ").length} words will be deleted.</p>
+          <div className={classes["button-container"]}>
+            <button onClick={modalHandler}>No</button>
+            <button onClick={deleteNotes}>Yes</button>
           </div>
         </div>
-      )
-    );
-  }
+      </div>
+    )
+  );
+}
--- src/components/AlertDialog/AlertDialogModalPortal.jsx
+++ src/components/AlertDialog/AlertDialogModalPortal.jsx
@@ -1,7 +1,12 @@
 import { createPortal } from "react-dom";
 import AlertDialogModal from "./AlertDialogModal";
 
-export default function AlertDialogModalPortal({ isModalOpen, modalHandler, textAreaValue }) {
+export default function AlertDialogModalPortal({
+  isModalOpen,
+  modalHandler,
+  textAreaValue,
+  deleteNotes
+}) {
   return (
     <>
       {createPortal(
@@ -9,6 +14,7 @@ export default function AlertDialogModalPortal({ isModalOpen, modalHandler, text
           isModalOpen={isModalOpen}
           modalHandler={modalHandler}
           textAreaValue={textAreaValue}
+          deleteNotes={deleteNotes}
         />,
         document.querySelector("#alter-dialog-modal-root")
       )}
