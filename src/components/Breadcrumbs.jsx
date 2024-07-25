183deefd0598e5e35d1f884a121bc12bc2d220ff
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-25 07:11:42 +0200

    Handling nested child routes

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,22 +1,28 @@
 import { Link, useMatches } from "react-router-dom";
 
+// ul 
+
 import classes from "./Breadcrumbs.module.scss"
 
 export default function Breadcrumbs() {
   const matches = useMatches();
   const crumbs = matches
     .filter((match) => Boolean(match.handle?.crumb))
-    .map((match) => match.handle.crumb(match.data));
+    .map((match) => ({
+      crumbName: match.handle.crumb(match.data),
+      pathname: match.pathname,
+    }));
 
   return (
     <ul className={classes["breadcrumb-wrapper"]}>
       {crumbs.map((crumb, index) => (
         <li key={index}>
           {index < crumbs.length - 1 ? (
-            <Link to={matches[index].pathname}>{crumb}</Link>
+            <Link to={crumb.pathname}>{crumb.crumbName}</Link>
           ) : (
-            <span>{crumb}</span>
+            <span>{crumb.crumbName}</span>
           )}
+          {index < crumbs.length - 1 && " > "}
         </li>
       ))}
     </ul>
