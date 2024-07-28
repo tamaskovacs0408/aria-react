b0432f1afe7450441ef94cff0fddee13771ec15d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-28 21:11:31 +0200

    Update buttons component

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,11 +1,18 @@
-import { NavLink } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import classes from "./Buttons.module.scss"
 
 export default function Buttons() {
+  const location = useLocation();
+  const isActionButtonPage = location.pathname.includes('/buttons/actionbutton');
+
   return (
-    <>
-        <NavLink to='actionbutton'>Action Button</NavLink>
-      <Outlet />
-    </>
+    <div className={classes["buttons-wrapper"]}>
+      {!isActionButtonPage && (
+        <NavLink className={classes.navlink} to='actionbutton'>Action Button</NavLink>
+      )}
+      <div className={classes["content-wrapper"]}>
+        <Outlet />
+      </div>
+    </div>
   );
 }
