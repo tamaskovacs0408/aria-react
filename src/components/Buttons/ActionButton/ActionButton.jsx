5fc57eeec67191d230c2a51293d2be1732a444af
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-29 20:30:59 +0200

    add logic to show selected action

--- src/components/Buttons/ActionButton/ActionButton.jsx
+++ src/components/Buttons/ActionButton/ActionButton.jsx
@@ -1,25 +1,40 @@
-import { useState } from "react";
-
-import classes from "./ActionButton.module.scss";
+import { useState } from 'react'
+import classes from './ActionButton.module.scss'
 
 export default function ActionButton() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedAction, setSelectedAction] = useState('')
 
   function handleButtonClick() {
-    setIsOpen(!isOpen);
+    setIsOpen(!isOpen)
+  }
+
+  function handleActionClick(e) {
+    setSelectedAction(e.target.getAttribute('data-value'))
   }
 
   return (
     <div className={classes.wrapper}>
-      <button className={classes.btn} onClick={handleButtonClick}>Actions</button>
+      <p>Last action: <span>{selectedAction}</span></p>
+      <button className={classes.btn} onClick={handleButtonClick}>
+        Actions
+      </button>
       {isOpen && (
-      <ul className={classes.actions}>
-        <li>Action 1</li>
-        <li>Action 2</li>
-        <li>Action 3</li>
-        <li>Action 4</li>
-      </ul>
-      )} 
+        <ul className={classes.actions}>
+          <li data-value='Action 1' onClick={handleActionClick}>
+            Action 1
+          </li>
+          <li data-value='Action 2' onClick={handleActionClick}>
+            Action 2
+          </li>
+          <li data-value='Action 3' onClick={handleActionClick}>
+            Action 3
+          </li>
+          <li data-value='Action 4' onClick={handleActionClick}>
+            Action 4
+          </li>
+        </ul>
+      )}
     </div>
-  );
+  )
 }
